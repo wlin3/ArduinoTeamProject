441b8ff8d3867f7feb2dbe550d69668d61a297c0
William Lin <02william.lin@gulllakecs.org>
2020-01-16 13:51:40 -0500

    Finish setting up LED code

--- do/do.ino
+++ do/do.ino
@@ -1,14 +1,37 @@
-/*
- * Programmers: Wyatt McComb, William Lin
- * Date: 1/6/20
- */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+int fingerprintID = 0;
+
+
+void setup(){
+  //Fingerprint sensor module setup
+  Serial.begin(9600);
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } 
+  else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  delay(50);
+  if(fingerprintID == 1 || fingerprintID == 3 || fingerprintID == 4 || fingerprintID == 5){
+           
+  }  
+  else if(fingerprintID == 2){
+         
+  }
 }
+
+// returns -1 if
